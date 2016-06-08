fd8c80435dfa3899ec6614b4da793994eb75092f
Andreja Jevtic <andrajevtic@gmail.com>
2016-06-08 15:11:10 -0700

    added Twilio messaging system

--- app.js
+++ app.js
@@ -15,11 +15,10 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var imageHandler = require('../handlers/imageHandler');
 var datastore = require('../databse/datastore');
+var config=require('../config.json');
+
+var client = require('twilio')(config.TWILIO_ACCOUNT_SID,config.TWILIO_AUTH_TOKEN);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,36 +18,53 @@ router.post(
         console.log("/upload");
         imageHandler.sendUploadToGCS(req, res, function (err, data) {
             var dataR = req.body;
-           // console.log("req.file: "+dataR);
             if (err || req.file === null || req.file === undefined) {
               /*  res.render('./image_upload/uploadError', {
                     errorType: "File upload error",
                     err:err
                 });
                 */
-                res.json({"response:":"error"});
+                res.json({"response":"error"});
             }else {
                 if (req.file && req.file.cloudStoragePublicUrl) {
                     dataR.imageUrl = req.file.cloudStoragePublicUrl;
                 }
+                /*
                 datastore.create(dataR, function (err, savedData) {
                     if (err) {
                        /* res.render('./image_upload/uploadError', {
                             errorType: "Database error",
                             err:err
-                        });*/
-                        res.json({"response:":"error"});
-
-                    } else {
+                        });
+                        res.json({"response":"error"});
+                    }
+                     */
                         console.log(JSON.stringify(req.body, null, 4));
                        // res.render('./image_upload/uploadSuccessful', {});
                         res.json([{"response":req.file.originalname}]);
-
-                    }
-
-                });
             }
         });
     });
+
+router.post(
+    '/sms',
+    function (req,res,next) {
+        console.log("SMS: "+req.body.message);
+        client.sms.messages.post({
+            to:'+14156919591',
+            from:config.TWILIO_NUMBER,
+            body:req.body.message
+    },function(err, text){
+            if(!err) {
+                res.json([{"response" : "Works"}]);
+                console.log('You sent: ' + text.body);
+                console.log('Current status of this text message is: ' + text.status);
+            }else{
+                console.log('Error: ' + err.body);
+
+            }
+        });
+    });
+
 
 module.exports = router;
